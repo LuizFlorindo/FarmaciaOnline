d50d21e6c1568fbe25fa038ff8ba76bae4bdd770
Luiz Da Silva <luizflorindo123@gmail.com>
2023-05-13 13:21:33 -0300

    website header creation

---
+++ dao/createBD.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE IF NOT EXISTS farmaciaOnline;
+
+USE farmaciaOnline;
+
+CREATE TABLE IF NOT EXISTS categoria (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS produto (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT NOT NULL,
+    quantidade INT NOT NULL,
+    preco FLOAT NOT NULL,
+    categoria_id INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS cesta (
+    id INT NOT NULL AUTO_INCREMENT,
+    usuario_id INT NOT NULL,
+    produto_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (produto_id) REFERENCES produto(id),
+    PRIMARY KEY (id)
+);
